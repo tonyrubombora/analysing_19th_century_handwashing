4e01e2960ab37520be2fa88b0e4d064885fe087b
= <=>
2020-07-30 11:49:31 +0300

    second commit: proportion of deaths & births

--- notebook.r
+++ notebook.r
@@ -4,3 +4,11 @@ library(tidyverse)
 yearly <- read_csv(datasets/yearly_deaths_by_clinic.csv)
 # Print out yearly
 print(yearly)
+
+# Adding a new column to yearly with proportion of deaths per no. births
+yearly <- yearly %>% 
+mutate(yearly, proportion_deaths = (deaths/births));
+
+# Print out yearly
+yearly
+
