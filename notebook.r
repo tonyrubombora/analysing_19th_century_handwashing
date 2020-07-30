bd5c2aa11ded5024b5688b12106e0ae360ad4fb6
= <=>
2020-07-30 11:28:50 +0300

    first commit: printing out the data

---
+++ notebook.r
@@ -0,0 +1,6 @@
+# Load in the tidyverse package
+library(tidyverse)
+# Read datasets/yearly_deaths_by_clinic.csv into yearly
+yearly <- read_csv(datasets/yearly_deaths_by_clinic.csv)
+# Print out yearly
+print(yearly)
